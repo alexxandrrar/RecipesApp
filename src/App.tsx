b00bc7af53db09d4dd8a679050e7cbf6d8439118
alexxandrrar <alexandroovnaa@gmail.com>
2023-12-28 17:19:33 +0200

    added Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Button from './components/common/Button';
+import Input from './components/common/Input';
 
 function App() {
   return (
     <div className="App">
       <Button size='large'>Hello</Button>
+      <Input></Input>
     </div>
   );
 }
