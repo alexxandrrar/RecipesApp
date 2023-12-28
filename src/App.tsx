e53fa968e8ea10e9f4607aa8733e964e1e332568
alexxandrrar <alexandroovnaa@gmail.com>
2023-12-28 18:28:23 +0200

    created home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import './App.css';
+import { routes } from './constants/path';
+import { Route,Routes } from 'react-router-dom';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        {routes.map(({ path,element: Element }) => (
+          <Route key={path} path={path} element={<Element />} />
+        ))}
+      </Routes>
     </div>
   );
 }
---
+++ src/constants/path.ts
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { Home } from '../pages/Home/index';
+
+export enum EPath {
+    HOME = '/',
+}
+
+type TPath = {
+    path: string;
+    element: () => React.ReactElement;
+};
+
+export const routes: TPath[] = [
+    {
+        path: EPath.HOME,
+        element: Home,
+    },
+];
