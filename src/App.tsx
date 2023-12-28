0ac7bdb683e2e09d0fb72484e16ce877a94ed573
alexxandrrar <alexandroovnaa@gmail.com>
2023-12-28 17:25:06 +0200

    added Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import Button from './components/common/Button';
-import Input from './components/common/Input';
 
 function App() {
   return (
     <div className="App">
-      <Button size='large'>Hello</Button>
-      <Input></Input>
     </div>
   );
 }
---
+++ src/components/common/Search/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { SearchInput } from 'evergreen-ui';
+import { getCombinedClassName } from '../../../utils/helpers';
+
+interface ISearch {
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    disabled?: boolean;
+    className?: string;
+    placeholder?: string;
+    width?: string | number;
+}
+
+const Search: React.FC<ISearch> = ({
+    value,
+    onChange,
+    disabled,
+    className,
+    placeholder,
+    width,
+}) => {
+    return (
+        <SearchInput
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+            className={getCombinedClassName('search',className)}
+            placeholder={placeholder}
+            width={width}
+        />
+    );
+};
+
+export default Search;
